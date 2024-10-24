e3d10d8e41f78b62d7a0f799f0c827bfd8b2bdf9
growwidtalha <growwithtalha2@gmail.com>
2024-10-24 15:22:12 +0500

    fixed

--- app/admin/products/[productId]/update/page.tsx
+++ app/admin/products/[productId]/update/page.tsx
@@ -2,8 +2,9 @@ import { getProductById } from '@/app/admin/_actions/product'
 import AddProductForm from '@/app/admin/_components/AddProductForm'
 import React from 'react'
 
-const ProductUpdatePage = async({params}: {params: {productId: string}}) => {
-    const product = await getProductById(params.productId)
+const ProductUpdatePage = async({params}: {params: any}) => {
+    const {productId} = await params
+    const product = await getProductById(productId)
   return (
     <div>
         <AddProductForm type="update" product={product}/>
