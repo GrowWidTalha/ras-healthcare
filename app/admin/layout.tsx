1c84a9b19fb622b57166d17062f15266900f4a9e
growwidtalha <growwithtalha2@gmail.com>
2024-10-24 22:34:18 +0500

    added admin auth

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,9 +1,11 @@
+import SecureAuthDialog from "@/components/AdminauthDialog"
 import { Sidebar } from "./_components/sidebar"
 
 const AdminLayout = ({ children } : { children: React.ReactNode }) => {
   return (
     <div className="flex">
         <Sidebar />
+        <SecureAuthDialog />
         <div className="flex-1 ml-0 md:ml-64">
             {children}
         </div>
