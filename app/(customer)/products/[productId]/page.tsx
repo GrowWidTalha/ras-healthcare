9744c7f2a034335fea17f30d66d6a826490ddefe
growwidtalha <growwithtalha2@gmail.com>
2024-10-24 15:15:44 +0500

    fixed

--- app/(customer)/products/[productId]/page.tsx
+++ app/(customer)/products/[productId]/page.tsx
@@ -1,14 +1,18 @@
-import { getProductById } from '@/app/admin/_actions/product';
-import ProductDetails from '@/components/ProductDetails';
-import React from 'react'
+import { getProductById } from "@/app/admin/_actions/product";
+import ProductDetails from "@/components/ProductDetails";
+import React from "react";
 
-const ProductDetailsPage = async({params: { productId}}: {params: { productId: string}}) => {
-    const product = await getProductById(productId);
+const ProductDetailsPage = async ({
+  params,
+}: {
+  params: { productId: string };
+}) => {
+  const product = await getProductById(params.productId);
   return (
-    <div className='container mx-auto'>
-        <ProductDetails product={product}/>
+    <div className="container mx-auto">
+      <ProductDetails product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
